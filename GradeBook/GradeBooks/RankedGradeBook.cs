1654da61d2744f9c940dfaf2aaeeced5ded11a55
mattias <mattias.nilsson2@eon.com>
2019-11-12 15:52:15 +0100

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -7,6 +9,50 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            double studentsInEachPercentile = Students.Count/5;
+            studentsInEachPercentile = Math.Floor(studentsInEachPercentile);
+
+            // Create a list of sorted grades.
+            ArrayList averageGrades = new ArrayList();
+            foreach (Student student in Students)
+            {
+                averageGrades.Add(student.AverageGrade);
+            }
+            averageGrades.Sort();
+
+            var higherGrades = 0;
+            foreach (double grade in averageGrades)
+            {
+                if (averageGrade < grade)
+                {
+                    higherGrades++;
+                }
+            }
+
+            var gradeValue = 5 - (Math.Floor(higherGrades/studentsInEachPercentile));
+
+            switch (gradeValue)
+            {
+                case 5:
+                    return 'A';
+                case 4:
+                    return 'B';
+                case 3:
+                    return 'C';
+                case 2:
+                    return 'D';
+            }
+            
+            return 'F';
         }
 
     }
